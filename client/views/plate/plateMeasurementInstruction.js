40b0651311bc4c80020a6907b21419e3b3e2a4c3
Michael Nagle <mpnagle@gmail.com>
2014-09-11 14:21:29 -0700

    adding plateMeasurementInstruction.js, 30 sec timer for plateMeasurement screen

---
+++ client/views/plate/plateMeasurementInstruction.js
@@ -0,0 +1,35 @@
+Template.plateMeasurementInstruction.created = function(){
+
+  Session.set("plateMeasurementCountdown",30);
+};
+
+
+var countOneSecond = function(){
+
+  var currentTime = Session.get("plateMeasurementCountdown");
+
+  Session.set("plateMeasurementCountdown",(Session.get("plateMeasurementCountdown") - 1));
+
+}
+
+
+    var intervalHandle;
+
+Template.plateMeasurementInstruction.rendered = function(){
+
+
+  intervalHandle = Meteor.setInterval(countOneSecond,1000);
+    
+
+} 
+
+Template.plateMeasurementInstruction.countdown = function(){
+    
+  if (Session.get("plateMeasurementCountdown") <= 0)
+  {
+    Meteor.clearInterval(intervalHandle);
+  }    
+  
+  return Session.get("plateMeasurementCountdown");
+
+}
