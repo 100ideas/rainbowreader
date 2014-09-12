e31b4b851343f9c74e3d04cf0ee0b464a57f9e8e
fgcapo <fgcapo@gmail.com>
2014-09-11 23:25:10 -0700

    server settings are development v2

--- server/lib/settings.js
+++ server/lib/settings.js
@@ -17,7 +17,7 @@ var settings = {
       "gphoto2": true,
       "opencfuPath": 'opencfu',
       "scannerPath": '/dev/usbscanner',
-      "platePhotosPath": "/photos/",
+      "platePhotosPath": "photos/",
       "fakeColonyDataFile": '/code/rainbowreader/test/colonyData.json',
       "fakeColonyPhotoFile": 'public/photos/small.jpg'
     }
