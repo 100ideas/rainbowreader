63e7e3d29504b2254fc0a626860fa64be3bec1f4
Mac Cowell <mac@has100ideas.com>
2014-09-25 16:02:58 -0700

    improving layout of plateview html

--- client/views/plate/platePhoto.js
+++ client/views/plate/platePhoto.js
@@ -4,16 +4,21 @@ Template.platePhoto.created = function () {
 
 Template.platePhoto.rendered = function () {
 
+  // trigger background umage update when photo us available
+  var photoURL = getSessionDocument().photoURL
+  if (photoURL) {
+    console.log("platePhoto.js: we've got a photo! " + photoURL);
+    changeBackgroundImg(photoURL);
+  }
+
   WorkstationSessions.find().observe({
     changed: function(newDocument, oldDocument) {
-      
-      // trigger background umage update when photo us available
-      if (newDocument.photoURL != oldDocument.photoURL) {
-        console.log("platePhoto.js: we've got a photo! " + newDocument.photoURL);
-        changeBackgroundImg(newDocument.photoURL);
-      }
 
-      // trigger reticules when colonyData field is updated
+      // TODO WTF weird - new and old doc _id and timestamps are always thes same?
+      // console.log("platePhoto.js:\t_id\t\t\t\t\tdateCreated\t\tuserBarcode\t\tphotoURL" +
+      //             "\n\toldDocument: " + oldDocument._id + "\t" + oldDocument.dateCreated + "\t" + oldDocument.userBarcode + "\t" + oldDocument.photoURL +
+      //             "\n\tnewDocument: " + newDocument._id + "\t" + newDocument.dateCreated + "\t" + newDocument.userBarcode + "\t" + newDocument.photoURL);
+      
       if (newDocument.colonyData && !oldDocument.colonyData) {
         console.log("platePhoto.js: observed a change in colonyData calling reticule animations");
         animateReticulesOnPlatePhoto();
@@ -23,6 +28,3 @@ Template.platePhoto.rendered = function () {
   })
 
 }
-
-
-changeBackgroundImg
