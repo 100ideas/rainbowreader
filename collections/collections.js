955085ca056e37b486b019eecce37cc4845966a2
Mac Cowell <mac@has100ideas.com>
2014-09-29 14:39:29 -0700

    turned off autosubscribe to detect when subs are ready

--- collections/collections.js
+++ collections/collections.js
@@ -1,5 +1,5 @@
 // document which stores the state of the current session
-WorkstationSessions = new Meteor.Collection('workstationSessions');
+WorkstationSessions = new Mongo.Collection('workstationSessions');
 
 // Holds the id of the mongo document which holds the state between server and client.
 // Is a string once the server creates the document, but Templates will not react at
@@ -7,7 +7,7 @@ WorkstationSessions = new Meteor.Collection('workstationSessions');
 workstationSession = {};
 
 // shared db of all data on plates on the visualization server
-Experiments = new Meteor.Collection('experiments');
+Experiments = new Mongo.Collection('experiments');
 
 // Holds environment variables
 // if comes after call to Visualizations and Visualizations is not defined, this collection
@@ -17,3 +17,20 @@ Admin = new Mongo.Collection('admin');
 
 Visualizations = new Mongo.Collection('visualizations');
 
+if (Meteor.isServer){
+  Meteor.publish('workstationSessions', function () {
+    return WorkstationSessions.find();
+  });
+
+  Meteor.publish('experiments', function () {
+    return Experiments.find();
+  });
+
+  Meteor.publish('admin', function () {
+    return Admin.find();
+  });
+
+  Meteor.publish('visualizations', function () {
+    return Visualizations.find();
+  });
+}
