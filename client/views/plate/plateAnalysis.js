a215c83ebe3c68f3cf0e071f82bf2a867b850cbb
Mac Cowell <mac@has100ideas.com>
2014-10-09 12:13:16 -0700

    big fonts but not completely responsive yet

--- client/views/plate/plateAnalysis.js
+++ client/views/plate/plateAnalysis.js
@@ -1,45 +1,31 @@
-
 //counts the number of coloinies in colonyData
 //this is just the length of the colonyData array
 Template.plateAnalysis.colonyCount = function(){
-
     var currentDoc = getSessionDocument();
     return currentDoc.colonyData.length;
 }
 
-
-
 //counting # of unique xkcd color labels
 Template.plateAnalysis.colorCount = function(){
-
     var currentDoc = getSessionDocument();
 
     //way to keep track of what hue values we've seen.
     var colorObject = {};
-
     currentDoc.colonyData.forEach(function(element){
-
 	    var color = element.ColorName;
-
 	    if (colorObject.hasOwnProperty[color]) { return; }
-
 	    colorObject[color] = 1;
 	});
-
+    
     //getting the number of elements in the object, which is the number of unique colors
     return Object.keys(colorObject).length;
-
 }
 
 
 Template.plateAnalysis.displayTime = function(){
-
     var currentDoc = getSessionDocument();
-
     var unixTime = currentDoc.dateCreated;
-
     var date = new Date(unixTime);
-
     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
     var year = date.getFullYear();
@@ -48,7 +34,6 @@ Template.plateAnalysis.displayTime = function(){
     var hour = date.getHours();
     var min = date.getMinutes();
     var sec = date.getSeconds();
-
 
     if (hour<10) hour +=0;
     if (min<10) min += 0;
@@ -59,33 +44,43 @@ Template.plateAnalysis.displayTime = function(){
 }
 
 Template.plateAnalysis.totalColonies = function(){
-
-  if (Visualizations.find().fetch().length)
-
-	{
-    var visualizationsArray = Visualizations.find().fetch();
-  	var statsObject = visualizationsArray[0];
-
-	  return statsObject.coloniesCount;
-
+  if (Visualizations.find().fetch().length) {
+    var stats = Visualizations.findOne({'id': 'stats'}) || {};
+	  return stats.coloniesCount;
+  }else{ 
+    return Meteor.settings.public.environment != "museum" ? "1232*" : "eColor & visualizations not found";
   }
-
-  else return "eColor & visualizations not found";
 }
 
-
-// ("#plate-stats-list-group li").velocity("transition.perspectiveDownIn", {
-//         duration: 1000,
-//         stagger: 400,
-//         drag: true})
-
-Template.plateAnalysis.statsListAnimation = function (){
-  if (Session.get("reticulesDone")) {
-    Meteor.setTimeout(function (){
-      $("#plate-stats-list-group ul").velocity("transition.perspectiveDownIn", {
-        duration: 1000,
-        stagger: 400,
-        drag: true
-      });
-  }, 500)}
+Template.plateAnalysis.colorCounts = function(){
+  if (Visualizations.find().fetch().length) {
+    var colorCounts = Visualizations.findOne({'id': 'colorCounts'}) || {};
+    var statsObject = {};
+    colorCounts = lodash(colorCounts).omit(["_id", "id"]);
+    statsObject.uniqueColors = colorCounts.size();
+    statsObject.totalColors = colorCounts.reduce(function(sum, num){return sum + num})  // same as coloniesCount, duh.
+    return statsObject;
+  }else{ 
+    if(Meteor.settings.public.environment != "museum"){
+      return {uniqueColors: "46*", totalColors: "1232*"}
+    }
+    return {uniqueColors: "eColor db not found", totalColors: "eColor db not found"}
+  }
 }
+
+Template.plateAnalysis.rendered = function(){
+  Meteor.setTimeout(function() {
+    $("#plate-stats-list-group li").velocity("transition.slideDownIn", {
+      duration: 1000, 
+      stagger: 300,
+      drag: true
+    });
+  }, 1000);
+}
+
+Tracker.autorun(function (c) {
+  if (Session.equals("reticulesDone", true)){
+    console.log("plateAnalysis autorun");
+  c.stop();
+  }
+});
--- client/views/plate/plateRareColors.js
+++ client/views/plate/plateRareColors.js
@@ -1,15 +1,3 @@
-Template.plateRareColors.listRareColors = function(){
-
-  var doc = getSessionDocument();
-
-  var rareColonies = doc.rareColorIndices.map(function(index) {
-    return doc.colonyData[index];
-  })
-
-  return rareColonies;
-
-};
-
 Template.plateRareColors.events({
   'click #rareColorsButton': function() {
     var transitionTime = Meteor.settings.public.reticuleDuration || 2000;
@@ -33,9 +21,41 @@ Template.plateRareColors.events({
   }
 })
 
+// Template.plateRareColors.rendered = function(){
+//   this.find('#rare-colors')._uihooks = {
+//     insertElement: function (node, next) {
+//       var $node = $(node);
+//       $node.insertBefore(next);
+//       console.log("rareColorCard uihooks insertElement: node:" + node );
+//       $node.velocity("transition.bounceRightIn", {
+//         // duration: ,
+//         // easing: ,
+//         complete: function () {
+//             $node = null;
+//         }
+//       });
+//     }
+//   }
+// }
 
-Template.rareColorCard.rendered = function(){
+
+Template.plateRareColors.animateIn = function(){
+  Meteor.setTimeout( function(){
+    $(".rareColorCard").velocity("transition.slideDownIn", {
+      duration: 1000, 
+      stagger: 300,
+      drag: true
+    });
+  }, 100);
 }
+
+Template.rareColorCard.listRareColors = function(){
+  var doc = getSessionDocument();
+  var rareColonies = doc.rareColorIndices.map(function(index) {
+    return doc.colonyData[index];
+  })
+  return rareColonies;
+};
 
 Template.rareColorCard.totalColonies = function(){
   return getSessionDocument().coloniesCountAtThisTime;
@@ -44,62 +64,22 @@ Template.rareColorCard.totalColonies = function(){
 // var hueToHsl = function(hue) {return "hsl(" + hue + ",50%,50%)";}
 
 Template.rareColorCard.idName = function(colorName){
-
   return colorName.replace(/\s+/g, ''); //removing spaces for it to be a valid ID
-
 }
 
-
-
-Template.plateRareColors.drawCircle2 = function(hue, saturation, colorName){
-
-  Meteor.setTimeout(function(){ //making sure previous template has loaded (waiting for the race condition.)
-
-  var id = colorName.replace(/\s+/g, '');
-
-  var saturationPercent = (saturation / 255) * 100;
-  saturationPercent = saturationPercent.toFixed(2);
-  var canvas = document.getElementById(id);
-      var context = canvas.getContext('2d');
-      var centerX = canvas.width / 2;
-      var centerY = canvas.height / 2;
-      var radius = canvas.width / 3;
-
-      context.beginPath();
-      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-      context.fillStyle = 'hsl(' + hue + ',' + saturationPercent + '%,50%)';
-      context.fill();
-      context.lineWidth = 2;
-      context.strokeStyle = '#003300';
-      context.stroke();
-
-   },10);
-}
-
-Template.plateRareColors.drawCircle = function(Rmean, Gmean, Bmean, colorName){
-  Meteor.setTimeout(function(){ //making sure previous template has loaded (waiting for the race condition.)
+Template.rareColorCard.drawCircle = function(Rmean, Gmean, Bmean, colorName){
+  Meteor.setTimeout( function(){
     var id = colorName.replace(/\s+/g, '');
-
     var color = d3.rgb(Rmean, Gmean, Bmean);
-
-    // TODO dynamically set size of .colony-ball to fit in user window (or just pick big vs small preset)
+    var h2Styles = { color: "rgb(" + color.r + "," + color.g + "," + color.b + ")"}
     var circleStyles = {
       "background-color": color.toString(),
       "box-shadow":
         "-5px 5px 30px rgba(" + color.r + "," + color.g + "," + color.b + "," + "0.5)," +
         "50px -50px 50px rgba(0, 0, 0, .2) inset"
     };
-    
     $("#" + id).css(circleStyles);
-
-    var h2Styles = {
-      color: "rgb(" + color.r + "," + color.g + "," + color.b + ")"
-    }
-
     $("#" + id + "+ div h1").css(h2Styles);
-
-   },10);
+  }, 10);
 }
-
-
 
